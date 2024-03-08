4ddbf8057bcd5a195c748da57646f603c89694bc
joaquin albanesi <joaquinalbanesidev@gmail.com>
2024-03-08 18:52:09 -0300

    feat: Firebase authentication service

---
+++ src/app/core/authentication/auth.service.ts
@@ -0,0 +1,77 @@
+import { Injectable, inject, signal } from "@angular/core";
+import {
+  Auth,
+  AuthProvider,
+  GoogleAuthProvider,
+  UserCredential,
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signInWithRedirect,
+  signOut,
+  updateProfile,
+  user,
+} from "@angular/fire/auth";
+import { UserInterface } from "../models/auth.model";
+import { Observable, from } from "rxjs";
+import { DeviceDetService } from "../services/device-det.service";
+
+@Injectable({
+  providedIn: "root",
+})
+export class AuthService {
+  firebaseAuth = inject(Auth);
+  deviceDetector = inject(DeviceDetService);
+  user$ = user(this.firebaseAuth);
+  currentUserSig = signal<UserInterface | null | undefined>(undefined);
+  userAuthSig = signal<boolean>(false);
+
+  public registerWithEmail(username: string, email: string, password: string): Observable<void> {
+    const promise = createUserWithEmailAndPassword(this.firebaseAuth, email, password).then((response) =>
+      updateProfile(response.user, { displayName: username }),
+    );
+    return from(promise);
+  }
+
+  public loginWithEmail(email: string, password: string): Observable<void> {
+    const promise = signInWithEmailAndPassword(this.firebaseAuth, email, password).then(() => {});
+    return from(promise);
+  }
+
+  public signInWithGoogle(): Promise<UserCredential> {
+    const provider = new GoogleAuthProvider();
+    return this.deviceDetector.isMobile() ? this.#callRedirect(provider) : this.#callPopUp(provider);
+  }
+
+  public sigInWithFacebook(): void {}
+
+  public sigInWithApple(): void {}
+
+  public async signOut(): Promise<void> {
+    try {
+      const auth = getAuth();
+      await signOut(auth);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async #callRedirect(provider: AuthProvider): Promise<UserCredential> {
+    try {
+      const auth = getAuth();
+      return signInWithRedirect(auth, provider);
+    } catch (error: any) {
+      return error;
+    }
+  }
+
+  async #callPopUp(provider: AuthProvider): Promise<UserCredential> {
+    try {
+      const result = await signInWithPopup(getAuth(), provider);
+      return result;
+    } catch (error: any) {
+      return error;
+    }
+  }
+}
