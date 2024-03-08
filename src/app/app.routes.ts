6c2f866ac7c58cd08457384fc6409031025acd2d
joaquin albanesi <joaquinalbanesidev@gmail.com>
2024-03-08 18:48:51 -0300

    routes: added login and register paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "login",
+    canActivate: [],
+    loadComponent: () => import("./core/authentication/login/login.component").then((x) => x.LoginComponent),
+    title: "login",
+  },
+  {
+    path: "register",
+    canActivate: [],
+    loadComponent: () => import("./core/authentication/login/login.component").then((x) => x.LoginComponent),
+    title: "register",
+  },
+];
