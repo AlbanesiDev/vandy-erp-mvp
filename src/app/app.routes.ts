c08c3a1fa26fc8135a2c8a4a24a6bdf4a261806f
joaquin albanesi <joaquinalbanesidev@gmail.com>
2024-03-11 23:42:04 -0300

    routes: Added dashboard path and canActivate

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from "@angular/router";
+import { authUserGuard } from "./core/guards/auth-user.guard";
 
 export const routes: Routes = [
   {
@@ -10,7 +11,19 @@ export const routes: Routes = [
   {
     path: "register",
     canActivate: [],
-    loadComponent: () => import("./core/authentication/login/login.component").then((x) => x.LoginComponent),
+    loadComponent: () => import("./core/authentication/register/register.component").then((x) => x.RegisterComponent),
     title: "register",
+  },
+  {
+    path: "dashboard",
+    canActivate: [authUserGuard],
+    loadComponent: () => import("./features/dashboard/dashboard.component").then((x) => x.DashboardComponent),
+    title: "Dashboard",
+  },
+  {
+    path: "**",
+    canActivate: [authUserGuard],
+    loadComponent: () => import("./features/dashboard/dashboard.component").then((x) => x.DashboardComponent),
+    title: "Dashboard",
   },
 ];
